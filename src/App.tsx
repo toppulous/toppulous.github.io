b453ffafefc13dfb17ce6ff2592edf011ce06d3d
nrtopping <nrtopping@gmail.com>
2020-05-12 00:03:21 -0700

    [Setup] test to see if deploy is working correctly

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 function App() {
   return (
     <Button variant="contained" color="primary">
-      Hello World
+      Hello World too
     </Button>
   );
 }
