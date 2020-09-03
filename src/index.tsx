c29315cd81b9b0371abeff9a85a0282e78138fe2
nrtopping <nrtopping@gmail.com>
2020-09-02 23:14:50 -0700

    whitespace changeC

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline/>
