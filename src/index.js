4b55395c382114753084d0347f6ee3d6e6dbbc0d
nrtopping <nrtopping@gmail.com>
2020-09-03 00:01:04 -0700

    Switch to Javascript

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme';
-
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline/>
+  <React.StrictMode>
     <App />
-  </ThemeProvider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
